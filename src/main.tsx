1a99075e30654c0648d37276dd816714f70c9693
nenlitiochristian <nenlitiochristian@gmail.com>
2024-07-11 21:41:15 +0700

    setup react routers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { RouterProvider } from "react-router-dom";
+
+import { router } from "./routes/Routes.tsx";
+
+export const API_START_URL = "http://localhost:8888";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />,
+);
