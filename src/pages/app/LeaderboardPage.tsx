e72919a5ff9d5859efd5470ed6c8f52ba0bbcc73
crzie <clashermature@gmail.com>
2024-07-12 11:20:08 +0700

    push leaderboard

--- src/pages/app/LeaderboardPage.tsx
+++ src/pages/app/LeaderboardPage.tsx
@@ -1,5 +1,45 @@
-const LeaderboardPage = () => {
-  return <div>Leaderboard</div>;
-};
+// import { Image } from "antd";
+// import { useState } from "react";
 
-export default LeaderboardPage;
+// type User = {
+//   name: string,
+//   exp: number
+// }
+
+// const LeaderboardPage = () => {
+//   const [topUsers, setTopUsers] = useState<User[]>([
+//     {
+//       name: "tio",
+//       exp: 10000
+//     },
+//     {
+//       name: "gaving",
+//       exp: 9999
+//     },
+//     {
+//       name: "carni",
+//       exp: 9000
+//     },
+//     {
+//       name: "devis",
+//       exp: 2999
+//     }
+//   ])
+
+//   // const LeaderboardItems = ({ rank, user }: { rank: number, user: User }) => {
+//   //   return (
+
+//   //   )
+//   // }
+
+//   return (
+//     <div className="p-10 flex flex-col items-center grow gap-3">
+//       <h1 className="text-center text-4xl font-bold">Leaderboard</h1>
+//       <h4 className="text-center text-xl tracking-widest">Top 100 sparkles</h4>
+
+//       {/* <Image src="" /> */}
+//     </div>
+//   )
+// };
+
+// export default LeaderboardPage;
