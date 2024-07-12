0e6a56c8e420ff7a58ab780bb089f50a1dc2cf90
gavindloka <lokagavindloka@gmail.com>
2024-07-12 17:45:09 +0700

    merge

--- src/pages/app/CreateEventPage.tsx
+++ src/pages/app/CreateEventPage.tsx
@@ -1,25 +1,123 @@
-import { Carousel } from 'antd';
-import banner1 from '../../assets/create_event_banners_1.jpg';
-import banner2 from '../../assets/create_event_banners_2.jpg';
-import banner3 from '../../assets/create_event_banners_3.jpg';
+import React, { useState } from "react";
+import { Button, Input, InputNumber, Upload, message } from "antd";
+import type { GetProp, UploadFile, UploadProps } from "antd";
+import ImgCrop from "antd-img-crop";
+import Title from "antd/es/typography/Title";
+import TextArea from "antd/es/input/TextArea";
+import { DatePicker } from "antd";
+const { RangePicker } = DatePicker;
+type FileType = Parameters<GetProp<UploadProps, "beforeUpload">>[0];
+import { UploadOutlined } from "@ant-design/icons";
+const CreateEventPage = () => {
+  const [banner, setBanner] = useState<UploadFile[]>([]);
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+  const onChange: UploadProps["onChange"] = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+  };
+  const onPreview = async (file: UploadFile) => {
+    let src = file.url as string;
+    if (!src) {
+      src = await new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file.originFileObj as FileType);
+        reader.onload = () => resolve(reader.result as string);
+      });
+    }
+    const image = new Image();
+    image.src = src;
+    const imgWindow = window.open(src);
+    imgWindow?.document.write(image.outerHTML);
+  };
+  const props: UploadProps = {
+    name: "file",
+    action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload",
+    headers: {
+      authorization: "authorization-text",
+    },
+    onChange(info) {
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === "done") {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+  };
 
-const contentStyle: React.CSSProperties = {
-    objectFit: 'cover',
-    objectPosition: '50% 45%',
-    width: '100%',
-    aspectRatio: '4',
+  return (
+    <div className="w-full h-full border-box p-8 ">
+      <div className="w-full h-full bg-white rounded-xl overflow-auto">
+        <form action="" className=" py-4 px-4">
+          <Title level={1}>Create Event</Title>
+          <div className="flex flex-col gap-3">
+            <div className=" w-full flex flex-col items-baseline">
+              {/* <ImgCrop rotationSlider>
+                <Upload
+                  action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+                  listType="picture-card"
+                  onChange={onChange}
+                  onPreview={onPreview}
+                >
+                  {banner.length < 1 && "+Upload Gallery"}
+                </Upload>
+              </ImgCrop> */}
+            </div>
+            <div className="w-3/5 flex flex-col items-baseline">
+              <Title level={5}>Event Name</Title>
+              <Input placeholder="Event Name" />
+            </div>
+            <div className="w-3/5 flex flex-col items-baseline">
+              <Title level={5}>Description</Title>
+              <TextArea rows={4} placeholder="Description" maxLength={500} />
+            </div>
+            <div className="w-3/5 flex flex-col items-baseline">
+              <Title level={5}>Bounty</Title>
+              <InputNumber addonAfter="XP" defaultValue={100} />
+            </div>
+            <div className="w-3/5 flex flex-col items-baseline">
+              <Title level={5}>Location</Title>
+              <Input placeholder="Location" />
+            </div>
+            <div className="w-3/5 flex flex-col items-baseline">
+              <Title level={5}>Select Date</Title>
+              <RangePicker />
+            </div>
+            <div className="">
+              <Title level={5}>Upload Gallery</Title>
+              <ImgCrop rotationSlider>
+                <Upload
+                  action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+                  listType="picture-card"
+                  fileList={fileList}
+                  onChange={onChange}
+                  onPreview={onPreview}
+                >
+                  {fileList.length < 5 && "+Upload Gallery"}
+                </Upload>
+              </ImgCrop>
+            </div>
+            <Button
+              size={"large"}
+              className=""
+              htmlType="submit"
+              style={{
+                backgroundColor: "#0B6A3C",
+                color: "white",
+                marginTop: "2vh",
+              }}
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
-
-const CreateEventPage = () => {
-
-    return (
-        <div className="w-full h-full border-box p-8 ">
-            <div className="w-full h-full bg-white rounded-xl overflow-auto">
-                
-            </div>
-        </div>
-    )
-}
-
-export default CreateEventPage
+export default CreateEventPage;
--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,57 +1,86 @@
 import { Button, Input, Typography } from "antd";
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import logo from '../../assets/SparkLogoTextUnder.png';
-import { auth } from '../../services/firebase';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "../../assets/SparkLogoTextUnder.png";
+import { auth } from "../../services/firebase";
 const { Title } = Typography;
 
 const SignIn = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = (event: { preventDefault: () => void; }) => {
-        event.preventDefault();
-        signInWithEmailAndPassword(auth, email, password)
-            .then(() => {
-                navigate('/');
-            })
-            .catch((error) => {
-                setError(error.message);
-            })
-    }
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
 
-    return (
-        <div className='w-screen h-screen flex justify-center items-center bg-green-white '>
-            <div className='w-3/4 h-3/4 bg-red-500 flex items-center justify-between rounded-2xl' style={{ backgroundColor: '#A1DEBB' }}>
-                <div className='w-1/2 h-full flex items-center justify-center'>
-                    <img className='w-100 h-1/2' src={logo} alt="" />
-                </div>
-                <form className='w-1/2 h-full bg-white flex flex-col justify-center' onSubmit={handleSubmit}>
-                    <p className='pb-5 w-full flex flex-col items-center text-5xl text-emerald-900 font-normal'>Login</p>
-                    <div className='flex flex-col items-center '>
-                        <div className='w-3/5 flex flex-col items-baseline'>
-                            <Title level={5} >Email</Title>
-                            <Input type='email' placeholder="Email" className='border-black '
-                                onChange={(e) => { setEmail(e.target.value) }} />
-                        </div>
-                        <div className='w-3/5 flex flex-col items-baseline'>
-                            <Title level={5} >Password</Title>
-                            <Input.Password placeholder="Password" type='password' className='border-black '
-                                onChange={(e) => { setPassword(e.target.value) }} />
-                        </div>
-                        {error && <p className='text-red-500 mt-4'>{error}</p>}
-                        <div className='w-3/5 flex flex-col items-center mt-4'>
-                            <Button type="primary" className='mb-3 w-1/2' htmlType="submit" style={{ backgroundColor: '#0B6A3C' }}>Login</Button>
-                            <Title level={5} >Don't have account? <a href="/signup">Register</a></Title>
-                        </div>
-                    </div>
-                </form>
-            </div>
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-green-white ">
+      <div
+        className="w-3/4 h-3/4 bg-red-500 flex items-center justify-between rounded-2xl"
+        style={{ backgroundColor: "#A1DEBB" }}
+      >
+        <div className="w-1/2 h-full flex items-center justify-center">
+          <img className="w-100 h-1/2" src={logo} alt="" />
         </div>
-    )
-}
+        <form
+          className="w-1/2 h-full bg-white flex flex-col justify-center"
+          onSubmit={handleSubmit}
+        >
+          <p className="pb-5 w-full flex flex-col items-center text-5xl text-emerald-900 font-normal">
+            Login
+          </p>
+          <div className="flex flex-col items-center ">
+            <div className="w-3/5 flex flex-col items-baseline">
+              <Title level={5}>Email</Title>
+              <Input
+                type="email"
+                placeholder="Email"
+                className="border-black "
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div className="w-3/5 flex flex-col items-baseline">
+              <Title level={5}>Password</Title>
+              <Input.Password
+                placeholder="Password"
+                type="password"
+                className="border-black "
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+            {error && <p className="text-red-500 mt-4">{error}</p>}
+            <div className="w-3/5 flex flex-col items-center mt-4">
+              <Button
+                type="primary"
+                className="mb-3 w-1/2"
+                htmlType="submit"
+                style={{ backgroundColor: "#0B6A3C" }}
+              >
+                Login
+              </Button>
+              <Title level={5}>
+                Don't have account? <a href="/signup">Register</a>
+              </Title>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
