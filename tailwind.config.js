26ef40159956636880de79979a4f0344c3c7d7d6
gavindloka <lokagavindloka@gmail.com>
2024-07-12 11:19:32 +0700

    merged changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        'nunito': ['Nunito', 'sans-serif'],
+      colors: {
+        bg1: "rgb(188, 0, 162)",
+        bg2: "rgb(0, 17, 82)",
+        color1: "rgb(18, 113, 255)",
+        color2: "rgb(221, 74, 255)",
+        color3: "rgb(180, 220, 255)",
+        color4: "rgb(200, 50, 50)",
+        color5: "rgb(180, 180, 50)",
+        interactive: "rgb(140, 100, 255)",
       },
       backgroundImage: {
-        'green-white': 'linear-gradient(90deg, rgba(27,138,82,1) 25%, rgba(35,155,93,1) 50%, rgba(255,255,255,1) 100%)',
+        "green-white":
+          "linear-gradient(90deg, rgba(27,138,82,1) 25%, rgba(35,155,93,1) 50%, rgba(255,255,255,1) 100%)",
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
