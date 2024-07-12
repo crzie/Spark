6345d01f7a6b1c428d0f4a861e30cb97b7066215
Daviskelvin824 <daviskelvin824@gmail.com>
2024-07-12 10:04:50 +0700

    sign in sign up

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import SignIn from "../pages/auth/SignIn";
+import SignUp from "../pages/auth/SignUp";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{}],
+    children: [
+      {path:'/signin',element:<SignIn/>},
+      {path:'/signup',element:<SignUp/>},
+
+    ],
   },
 ]);
